140b6082771a2d9426df10e62013690cb84554dd
jonny652 <jonny_uk@live.co.uk>
2026-06-25 14:36:32 +0100

    Add Playwright test for navigating to the Dyson homepage

---
+++ tests/first-test.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('navigate to the dyson homepage', async ({ page }) => {
+  await page.goto('https://source.thenbs.com/en/gb');
+  await page.getByRole('button', { name: 'Close dialog' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).fill('dyson');
+  await page.getByRole('textbox', { name: 'Search' }).press('Enter');
+  await page.getByRole('tab', { name: 'Manufacturers' }).click();
+  await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
+});
