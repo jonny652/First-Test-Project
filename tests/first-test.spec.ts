f51baec0c70e31ceafbeafacd9280ef255e34ca2
jonny652 <jonny_uk@live.co.uk>
2026-06-26 12:09:38 +0100

    Add test to assert heading correctness on Dyson homepage

--- tests/first-test.spec.ts
+++ tests/first-test.spec.ts
@@ -10,3 +10,16 @@ test('navigate to the dyson homepage', async ({ page }) => {
   await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
   await expect(page).toHaveURL("https://source.thebs.com/en/gb/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview");
 });
+
+test('assert that the heading is correct on the dyson homepage', async ({ page }) => {
+  await page.goto('https://source.thenbs.com/en/gb');
+  await page.getByRole('button', { name: 'Close dialog' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).fill('dyson');
+  await page.getByRole('textbox', { name: 'Search' }).press('Enter');
+  await page.getByRole('tab', { name: 'Manufacturers' }).click();
+  await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
+  await expect(page).toHaveURL("https://source.thebs.com/en/gb/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview");
+
+  
+});
